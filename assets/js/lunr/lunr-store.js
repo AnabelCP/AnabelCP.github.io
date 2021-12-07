55c1e15a30d297b2407c83c376afafb5472092e5
AnabelCP <alu0100885200@ull.edu.es>
2021-12-07 22:13:37 +0000

    new-deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Docencia 1. Bienvenidos",
+        "excerpt":"Jueves, 4 de noviembre de 2021 Blah Hoy es el primer día en el que conocemos el funcionamiento de Jekyll. Hemos tenido algunos problemas informáticos en la anterior clase con el cañón, aunque afortunadamente, hoy ha funcionado. El título que he puesto a este post es Docencia 1. Bienvenidos, ya...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/Mi-primer-Post.-Docencia/",
+        "teaser": null
+      },{
+        "title": "Post de Prueba",
+        "excerpt":"Graduada en Ingeniería Agrícola y del Medio Rural Aprendiendo Me gusta mucho comer naranjas Me gusta mucho comer peras Me gusta mucho comer limones Me gusta mucho comer fresas Martes, 9 de noviembre de 2021 un título un título más pequeño Somos 20 alumnos Mi usuario de twitter es La...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/Post-de-prueba/",
+        "teaser": null
+      },{
+        "title": "Docencia 2. Iniciándonos en LATEX",
+        "excerpt":"Martes, 16 de noviembre de 2021 Primera fórmula LATEX \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] Vamos a realizar una tabla {“First name”=&gt;”John”, “Last name”=&gt;”Doe”, “Age”=&gt;”35”, “Location”=&gt;”United States”}{“First name”=&gt;”Jane”, “Last name”=&gt;”Doe”, “Age”=&gt;”29”, “Location”=&gt;”France”}{“First name”=&gt;”Jack”, “Last name”=&gt;”Hill”, “Age”=&gt;”25”, “Location”=&gt;”Australia”} Tabla mejorada First name Last name Age Location John...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/Docencia-2.-Clase/",
+        "teaser": null
+      },{
+        "title": "Post 2. Hambre Cero. Naciones Unidas. FAO",
+        "excerpt":"Martes, 16 de noviembre de 2021 Querido lector, ¿conoces los Objetivos de Desarrollo Sostenible? Seguramente sí, no obstante te haré un breve recordatorio (la información procede de este link ). En el año 2015, 193 Estados miembros de las Naciones Unidas adoptaron una serie de objetivos para erradicar la probreza,...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/Segundo-Post/",
+        "teaser": null
+      },{
+        "title": "Post 1. ¿Quién escribe este blog?",
+        "excerpt":"Lunes, 15 de noviembre de 2021 Querido lector, te doy la bienvenieda a este nuevo blog. Me llamo Anabel, soy graduada en Ingeniería Agrícola y del Medio Rural, y actualmente estoy realizando el Máster de Formación del Profesorado, de hecho este blog es una actividad que forma parte de la...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/Qui%C3%A9n-soy/",
+        "teaser": null
+      },{
+        "title": "Docencia 3. LATEX. Integración curricular y el número de oro",
+        "excerpt":"Martes, 30 de noviembre de 2021 En este tercer post de docencia, hablaremos de: la integración curricular y el número áureo. ¿Qúe es la integración curricular? La integración curricular se basa en la interconexión de disciplinas, es decir, es una forma de diseñar el curriculum fundamentado en una concepción holística...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/Docencia-3.-LATEX/",
+        "teaser": null
+      },{
+        "title": "Docencia 4. El huerto escolar en la Educación Secundaria Obligatoria",
+        "excerpt":"Jueves, 2 de diciembre de 2021 En este cuarto post de docencia, hablaremos del huerto escolar en la ESO. ¿Huerto escolar? El huerto escolar es un recurso educativo fantástico, ya que, se adapta a todas las etapas educativas y a muchos de los contenidos recogidos en el curriculum. Según García,...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/Docencia-4.-El-huerto-escolar/",
+        "teaser": null
+      }]
